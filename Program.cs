35f3aff0bea84fd74dc5abb7a58ca4d2c918a5d7
Joao Nakano <joaovynak@gmail.com>
2025-09-10 17:51:39 -0300

    initial program file upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<String, string[]> diasDaSemana = new()
+{
+    ["segunda"] = ["Sala 1, Bloco 1", "Sala 2, CT"],
+    ["terca"]   = ["Sala 2, CT", "Sala 2, CT"],
+    ["quarta"]  = ["Sala 1, Bloco 1", "Sala 2 CT"],
+    ["quinta"]  = ["", ""],
+    ["sexta"]   = ["Sala 3, Bloco 4", "Sala 4, CT"],
+};
+
+string? userInput;
+
+do
+{
+    Console.WriteLine("Bem-vindo(a) ao meu programa de listagem de ensalamento!\nDigite um dia da semana para obter o ensamento:");
+    userInput = Console.ReadLine();
+
+    if (userInput != null)
+    {
+        string userInputFiltered = userInput.Trim();
+        if (diasDaSemana.ContainsKey(userInputFiltered))
+        {
+            Console.WriteLine("\n============= Encontrou! =============");
+            Console.WriteLine(string.Join(" | ", diasDaSemana[userInputFiltered]));
+            Console.WriteLine("======================================\n");
+        }
+        else
+        {
+            Console.WriteLine("\n============= Nao Encontrou! ============= ");
+            Console.WriteLine($"\"{userInputFiltered.GetType().FullName}\"");
+            Console.WriteLine("==========================================\n");
+        }
+    }
+} while (userInput != null);
